7c8f7e5bd44988d3b6edf14c8ea84d193c690c32
Seanli2013 <bigbelledu@gmail.com>
2014-11-21 13:13:03 -0800

    Initial Commit

---
+++ BBSelectImagesAndText/BBSelectImagesAndText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBSelectImagesAndText
+//
+//  Created by Xiang LI on 11/21/14.
+//  Copyright (c) 2014 Xiang LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBSelectImagesAndText/BBSelectImagesAndText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBSelectImagesAndText
+//
+//  Created by Xiang LI on 11/21/14.
+//  Copyright (c) 2014 Xiang LI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
